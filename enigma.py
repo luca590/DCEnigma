15685ac2628b4ddee0c327d5fe3f2f63542a650b
luca590 <lucasgaylord@protonmail.com>
2018-08-10 15:37:11 -0700

    adding enigma program

---
+++ enigma.py
@@ -0,0 +1,13 @@
+
+R1 = ['C','N','P','Y','S','W','O','I','T','Q','G','F','Z','J','L','E','D','A','U','X','R','V','B','H','K','M']
+R2 = ['X','U','E','C','Q','G','I','N','H','P','F','V','M','J','O','L','W','S','T','Y','R','A','D','Z','K','B']
+R3 = ['Y','R','I','M','L','E','J','D','T','C','P','O','N','W','Z','B','X','G','U','S','K','Q','V','F','A','H']
+
+R1ArrowIn = 'K'
+R2ArrowIn = 'B'
+R3ArrowIn = 'G'
+
+reflector = [
+
+def setp1():
+    
